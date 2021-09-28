ade8477460d5ce296d0d719e0b380e6a53232812
yxs <asong0122@163.com>
2021-09-28 23:01:30 +0800

    登录页面添加及功能完善

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,74 +1,45 @@
 <template>
-  <div class="login_box">
-    <el-form
-      class="login_form"
-      :model="loginForm"
-      :rules="loginFormRules"
-      ref="loginFormRef"
-    >
-      <el-form-item prop="username">
-        <el-input
-          v-model="loginForm.username"
-          prefix-icon="el-icon-user-solid"
-        ></el-input>
-      </el-form-item>
-      <el-form-item prop="password">
-        <el-input
-          v-model="loginForm.password"
-          prefix-icon="el-icon-lock"
-          type="password"
-        ></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="login">登录</el-button>
-        <el-button type="info" @click="resetLoinForm">重置</el-button>
-      </el-form-item>
-    </el-form>
+  <div class="login">
+    <div class="login_box">
+      <el-form
+          class="login_form"
+          :model="loginForm"
+          :rules="loginFormRules"
+          ref="loginFormRef">
+        <el-form-item prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="el-icon-user-solid"/>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="el-icon-lock"
+            type="password"/>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoinForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, toRefs, ref } from "vue";
-import axios from "axios";
-import router from "../router";
-import { ElMessage } from "element-plus";
-function useRestLog(loginFormRef) {
-  const resetLoinForm = () => {
-    loginFormRef.value.resetFields();
-  };
-  return {
-    resetLoinForm,
-  };
-}
-function useLogin(loginFormRef, state) {
-  const login = () => {
-    loginFormRef.value.validate(async (valid) => {
-      if (!valid) return;
-      const params = {
-        userName: state.loginForm.username,
-        userPwd: state.loginForm.password,
-      };
-      const res = await axios.post("/user/login", params);
-      if (res.data.code === 0) {
-        ElMessage.success("登录成功");
-        window.sessionStorage.setItem("token", res.data.myToken);
-        router.push("/");
-      } else {
-        ElMessage.error("登录失败");
-      }
-    });
-  };
-  return {
-    login,
-  };
-}
+import { reactive, toRefs, ref } from "vue"
+import axios from "axios"
+import {useRouter} from "vue-router"
+import { ElMessage } from "element-plus"
+
 export default {
   setup() {
+    const router = useRouter()
     const loginFormRef = ref();
     const state = reactive({
       loginForm: {
-        username: "",
-        password: "",
+        username: "admin",
+        password: "123456"
       },
     });
     const loginFormRules = {
@@ -78,8 +49,8 @@ export default {
           min: 3,
           max: 10,
           message: "用户名的长度在3到10个字符之间",
-          trigger: "blur",
-        },
+          trigger: "blur"
+        }
       ],
       password: [
         { required: true, message: "请输入用户密码", trigger: "blur" },
@@ -87,28 +58,55 @@ export default {
           min: 6,
           max: 15,
           message: "用户密码的长度在6到15个字符之间",
-          trigger: "blur",
-        },
-      ],
-    };
+          trigger: "blur"
+        }
+      ]
+    }
+    // 重置
+    const resetLoinForm = () => {
+      loginFormRef.value.resetFields()
+    }
+    // 登录
+    const login = () => {
+      loginFormRef.value.validate(async (valid) => {
+        if (!valid) return
+        const params = {
+          userName: state.loginForm.username,
+          userPwd: state.loginForm.password
+        };
+        const res = await axios.post("/user/login", params)
+        if (res.data.code === 0) {
+          ElMessage.success("登录成功");
+          window.sessionStorage.setItem("token", res.data.myToken)
+          router.push("/")
+        } else {
+          ElMessage.error("登录失败")
+        }
+      })
+    }
     return {
       ...toRefs(state),
       loginFormRules,
       loginFormRef,
-      ...useRestLog(loginFormRef),
-      ...useLogin(loginFormRef, state),
-    };
-  },
-};
+      resetLoinForm,
+      login
+    }
+  }
+}
 </script>
 
 <style>
+.login {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 .login_box {
   width: 450px;
-  height: 300px;
   background-color: #fff;
   border-radius: 3px;
-  margin: 0 auto;
 }
 .login_form {
   width: 100%;
